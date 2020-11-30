a59842b877f5b0c680361156fb447174c047ca08
tilseb <till@energymodellinglab.com>
2020-11-30 10:03:26 +0100

    Testing automated scenarioCombinations.js

--- src/data/scenarioCombinations.js
+++ src/data/scenarioCombinations.js
@@ -1,34 +1,68 @@
-﻿export default {
-  scenarioCombinations : 
-    {
-      scenarioOptions : [ 
-        { 
-          "id": 0, 
-          "name": "CNS", //name must follow optionAvailable order hence _cns_bio, not _bio_cns
-          "nameNoOptions": "CNS", 
-          "short_description": "TBase", //for translation: must be defined in src/translations/
-          "ultra_short_description": "B1",
-          "desc":"times_desc", //used for mouseover in scenario list, for translation: must be defined in src/translations/
-          "cns": false, //must correspond to name
-          "bio": false  //must correspond to name
-        },
-        { "id": 1, "name": "CNS_bio", "nameNoOptions": "CNS", "short_description": "cns + bio", "ultra_short_description": "Tech","desc":"nordic_tech_desc", "cns": true, "bio": true },
-        { 
-          "id": 3, 
-          "name": "CNS_copy", //name must follow optionAvailable order hence _cns_bio, not _bio_cns
-          "nameNoOptions": "CNS_copy", 
-          "short_description": "TBase2", //for translation: must be defined in src/translations/
-          "ultra_short_description": "B2",
-          "desc":"times_desc", //used for mouseover in scenario list, for translation: must be defined in src/translations/
-          "cns": false, //must correspond to name
-          "bio": false  //must correspond to name
-        },
-        { "id": 5, "name": "CNS_copy_Bio", "nameNoOptions": "CNS_copy", "short_description": "cns + bio", "ultra_short_description": "Tech","desc":"nordic_tech_desc", "cns": true, "bio": true },
-        
-      ], 
-      optionsAvailable:  { //option names are defined in src/translations/
-        "CNS": {"cns": false, "bio": true, "opt2": false, "opt3": false},
-        "CNS_copy": {"cns": false, "bio": true, "opt2": false, "opt3": false},
-      }
-    }
-  };
+export default { 
+	scenarioCombinations : 
+		{ 
+			scenarioOptions : [ 
+				{ 
+					"id": 0, 
+					"name": CNS_Bio, 
+					"nameNoOptions": CNS, 
+					"short_description": CNS + Bio, 
+					"ultra_short_description": C0, 
+					"desc": times_desc, 
+					"opt0": false, 
+					"opt1": true, 
+					"opt2": false, 
+					"opt3": false, 
+				}, 
+				{ 
+					"id": 1, 
+					"name": CNS, 
+					"nameNoOptions": CNS, 
+					"short_description": CNS, 
+					"ultra_short_description": C1, 
+					"desc": times_desc, 
+					"opt0": false, 
+					"opt1": false, 
+					"opt2": false, 
+					"opt3": false, 
+				}, 
+				{ 
+					"id": 2, 
+					"name": CNS_copy, 
+					"nameNoOptions": CNS, 
+					"short_description": CNS, 
+					"ultra_short_description": C2, 
+					"desc": times_desc, 
+					"opt0": false, 
+					"opt1": false, 
+					"opt2": false, 
+					"opt3": false, 
+				}, 
+				{ 
+					"id": 3, 
+					"name": CNS_copy_Bio, 
+					"nameNoOptions": CNS, 
+					"short_description": CNS + Bio, 
+					"ultra_short_description": C3, 
+					"desc": times_desc, 
+					"opt0": false, 
+					"opt1": true, 
+					"opt2": false, 
+					"opt3": false, 
+				}, 
+			], 
+			optionsAvailable: 
+				"CNS": { 
+					"opt0": false,
+					"opt1": true,
+					"opt2": false,
+					"opt3": false,
+					}, 
+				"CNS_copy": { 
+					"opt0": false,
+					"opt1": true,
+					"opt2": false,
+					"opt3": false,
+					}, 
+		} 
+	};
