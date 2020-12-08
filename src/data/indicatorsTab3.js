fd111127e3304978b6a6c1a8f19aab7128853efb
tilseb <till@energymodellinglab.com>
2020-12-08 18:06:46 +0100

    Revert "Add marginal prices to stackedBarTab3.js"
    This reverts commit e17ba59bc7ec3ec3300f21171e846b42bb4568cb.

--- src/data/indicatorsTab3.js
+++ src/data/indicatorsTab3.js
@@ -1,8 +1,6 @@
 export default 
  	 [ 
 		"District heat production (PJ)",
-		"Marginal Prices - District Heat (MEUR/PJ)",
-		"Marginal Prices - Electricity (MEUR/PJ) ",
 		"Power & Heat fuel consumption (PJ)",
 		"Power Trade (PJ)",
 		"Power capacity (MW)",
