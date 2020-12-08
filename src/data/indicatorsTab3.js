e17ba59bc7ec3ec3300f21171e846b42bb4568cb
tilseb <till@energymodellinglab.com>
2020-12-08 17:48:16 +0100

    Add marginal prices to stackedBarTab3.js

--- src/data/indicatorsTab3.js
+++ src/data/indicatorsTab3.js
@@ -1,6 +1,8 @@
 export default 
  	 [ 
 		"District heat production (PJ)",
+		"Marginal Prices - District Heat (MEUR/PJ)",
+		"Marginal Prices - Electricity (MEUR/PJ) ",
 		"Power & Heat fuel consumption (PJ)",
 		"Power Trade (PJ)",
 		"Power capacity (MW)",
