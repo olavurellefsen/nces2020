c684ba0ae3053f3af3f57b6174d84d4f5bf49c4f
tilseb <till@energymodellinglab.com>
2020-12-16 18:25:53 +0100

    Delete data for Tab7

--- src/data/indicatorsTab7.js
+++
@@ -1,9 +0,0 @@
-export default 
- 	 [ 
-		"Freight transport demand (mill.tkm)",
-		"Fuel consumption cars (PJ)",
-		"Fuel consumption transport (PJ)",
-		"Passenger transport demand (mill.pkm)",
-		"Transport CO2 emissioner (PJ)",
-		"Transport System Cost (MEUR15)",
-	 ]
