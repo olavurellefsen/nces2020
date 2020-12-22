b12068854ff96c6ffef11fad62b854f9caf6e4c1
tilseb <till@energymodellinglab.com>
2020-12-22 10:44:52 +0100

    Bug fix in scenarioCombinations.js

--- src/data/scenarioCombinations.js
+++ src/data/scenarioCombinations.js
@@ -24,6 +24,28 @@ export default {
 					"opt1": false, 
 					"opt2": false, 
 				}, 
+				{ 
+					"id": 2, 
+					"name": "CNN_Bio", 
+					"nameNoOptions": "CNN", 
+					"short_description": "CNN + Bio", 
+					"ultra_short_description": "CNN+B", 
+					"desc": "long and eloquent scenario description", 
+					"opt0": false, 
+					"opt1": true, 
+					"opt2": false, 
+				}, 
+				{ 
+					"id": 3, 
+					"name": "CNN_copy_Bio", 
+					"nameNoOptions": "CNN_copy", 
+					"short_description": "CNN + Bio", 
+					"ultra_short_description": "CNN+B", 
+					"desc": "long and eloquent scenario description", 
+					"opt0": false, 
+					"opt1": true, 
+					"opt2": false, 
+				}, 
 			], 
 			optionsAvailable: { 
 				"CNN": { 
