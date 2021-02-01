8b9ea25fe9882641b200c633fda4bd101b12ba2f
tilseb <35695265+tilseb@users.noreply.github.com>
2021-02-01 17:51:49 +0100

    Update indicatorsTab4.js

--- src/data/indicatorsTab4.js
+++ src/data/indicatorsTab4.js
@@ -2,11 +2,11 @@ export default
  	 [ 
 		"Heavy Industry CO2 emissions",
 		"Heavy Industry Energy service demand",
-		"Heavy Industry Fuel consumption",
+		"Heavy industry Fuel consumption",
 		"Cement fuel consumption",
 		"Iron and steel fuel consumption",
 		"Pulp and paper fuel consumption",
 		"Mining fuel consumption",
 		"Non-ferrous fuel consumption",
-		"Heavy industry annual costs",
+		"Heavy Industry annual costs",
 	 ]
