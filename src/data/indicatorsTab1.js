cf0a6a89eb5265e8fc5ca6fefcb3e8bb6288d3c3
tilseb <till@energymodellinglab.com>
2021-02-08 14:24:57 +0100

    Add new data and update indicatorsTabs 1+3

--- src/data/indicatorsTab1.js
+++ src/data/indicatorsTab1.js
@@ -1,6 +1,7 @@
 export default 
  	 [ 
 		"Primary energy supply by energy source",
+		"Marginal prices - CO2",
 		"CO2 emissions",
 		"Carbon capture",
 		"Final energy consumption by energy source",
