1977299c5d2444306c4078a56b2a4d921f70011d
Seregfaroth <bl@tokni.com>
2021-06-14 15:41:47 +0100

    a litlle oopsie fixed

--- src/alert/Welcome.js
+++ src/alert/Welcome.js
@@ -127,9 +127,6 @@ const welcomeText = {
 	  "welcome5": <p>But the CNB scenarios differs as demand for transport are not assumed to increase in future. Therefore, try to select CNN and CNB and compare the need for cars.</p>,
   },
   "tab8": {
-	  "welcome1": <p>Key Progress Indicators.</p>
-  },
-  "tab9": {
 	  "welcome1": <p>Key Performance Indicators.</p>,
 	  "welcome2": <p>Here we have chosen some indicators that can say something about the development of the Nordic energy system. The indicators show development in renewable energy shares, electrification of end-use energy consumption and energy intensity of industries. The indicators illustrate differences in the structure of the energy system across the Nordic countries.</p>,
 	  "welcome3": <p>Try to select the same scenario twice and see how increase price of CO<sub>2</sub> storage or limit on import of biomass influence the indicators.</p>,
