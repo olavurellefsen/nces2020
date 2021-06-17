d7d522abb37f4523bf385a5806552009b9a515ca
tilseb <till@energymodellinglab.com>
2021-06-17 14:26:36 +0200

    Change "&" to "and" in chart name

--- src/data/indicatorsTab5.js
+++ src/data/indicatorsTab5.js
@@ -4,7 +4,7 @@ export default
 		"Other sectors CO2 emissions by sector",
 		"Other sectors service demand",
 		"Other sectors fuel consumption",
-		"Agriculture & Fishery fuel consumption",
+		"Agriculture and Fishery fuel consumption",
 		"Manufacturing fuel consumption",
 		"Trade and service fuel consumption",
 	 ]
