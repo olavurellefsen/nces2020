30ab2b87c183b6e18685b59d2b8c6b3dcdd2dbcc
tilseb <till@energymodellinglab.com>
2021-07-01 10:39:51 +0200

    Prioritize down Marginal CO2 price figure

--- src/data/indicatorsTab1.js
+++ src/data/indicatorsTab1.js
@@ -1,9 +1,9 @@
 export default 
  	 [ 
 		"Primary energy supply by energy source",
-		"Marginal prices - CO2",
 		"CO2 emissions",
 		"Carbon capture",
 		"Final energy consumption by energy source",
 		"Final energy consumption by sector",
+		"Marginal prices - CO2",
 	 ]
