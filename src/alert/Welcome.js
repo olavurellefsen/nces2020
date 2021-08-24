734665f78b4758d7b64fec5480300e1b6e727096
Seregfaroth <bl@tokni.com>
2021-08-24 13:02:15 +0100

    Updates Welcome box

--- src/alert/Welcome.js
+++ src/alert/Welcome.js
@@ -16,6 +16,7 @@ const AlertContainer = styled.div`
   position: ${props => props.isOpen ? 'relative' : 'absolute'};
   right: ${props => props.isOpen ? null : '35px'};
   padding: 10px;
+  padding-left: 15px;
   margin-bottom: 10px;
   margin-right: 15px;
   ${'' /* border-width: 1px;
@@ -78,51 +79,51 @@ const CloseWindowIcon = styled.div`
 CloseWindowIcon.displayName = "CloseWindowIcon";
 const welcomeText = {
   "tab1": {
-    "welcome1": <p>Welcome to the Nordic Clean Energy Scenarios results viewer</p>,
+    "welcome1": <div>Welcome to the Nordic Clean Energy Scenarios results viewer</div>,
 	"welcome2": <p>On this webpage you can explore the full modelling results from the Nordic Clean Energy Scenarios project, funded by Nordic Energy Research.</p>,
 	"welcome3": <p>This web tool allows you to compare the results from three different scenarios developed by the project, view the effects on the energy system when applying additional assumptions to the model, and delve deeper into the full downloadable data provided in the statistics database and model results. For further instructions on how to use the web tool and get the most out of the presented results please see, <a href="./about" style={{color: "white"}}>How to use the results viewer tool</a>. To learn more about the project and access the full report please see <a href="./how-to-use" style={{color: "white"}}>About Nordic Clean Energy Scenarios.</a></p>,
 	"welcome4": <p>All NCES scenarios reach carbon neutrality by 2050, but technology choice, fuel consumption, and cost will adapt to the options available to change assumptions on the technological readiness of CCS technologies and limitations on bioenergy availability. </p>,
 	"welcome5": <p>The results presented here are highly dependent on assumptions for technology developments and their potential availability, efficiency, and cost to just mention a few factors influencing future pathway developments. The many combinations of scenarios represented in this tool demonstrates the inherent uncertainty of using scenario modelling to predict the future Nordic energy system. What these results tell us is rather which solutions are competing, what synergies exists between low carbon energy technologies, and what factors their development is contingent on.</p>,
   },
   "tab2": {
-    "welcome1": <p>The upstream sector includes oil and gas production, fossil refineries, biorefineries and PtX plants.</p>,
+    "welcome1": <div>The upstream sector includes oil and gas production, fossil refineries, biorefineries and PtX plants.</div>,
     "welcome2": <p>The total used biomass in the Nordic countries is tracked and also import of biomass and biofuels. Feedstock and produced fuel from both fossil based refineries and renewable based refineries are also shown.</p>,
     "welcome3": <p>Try to choose the same scenario twice and then activate the bioenergy constraint, the “leaf”, on one of the scenarios – then you can see a big difference in imported bioenergy and input to the RE-refineries.</p>,
   },
   "tab3": {
-    "welcome1": <p>Power and district heat production.</p>,
+    "welcome1": <div>Power and district heat production.</div>,
     "welcome2": <p>Here you can see the fuel use for power and district heat production, power plant capacities and the electricity and district heating production divided on fuels.</p>,
     "welcome3": <p>The marginal electricity price and district heating price is the weighted average price of the time slices in the model over a year. These prices are endogenous calculated by the model and it is the prices which the model “trade” power and heat between sectors.</p>,
     "welcome4": <p>Try to select the CNN scenario and the NPH scenario and compare electricity demand and the difference in needed power capacity.</p>,
   },
   "tab4": {
-	  "welcome1": <p>Heavy industry energy consumption.</p>,
+	  "welcome1": <div>Heavy industry energy consumption.</div>,
 	  "welcome2": <p>Here you can investigate energy consumption and CO<sub>2</sub>-emissions from heavy industry.</p>,
 	  "welcome3": <p>In heavy industry we include: Pulp and paper, Mining, Iron and steel, Aluminium and Cement.</p>,
 	  "welcome4": <p>Try to select the CNN scenario and the NPH scenario and you will see the assumed increase in activity in NPH – if you choose to compare with CNB instead you can see the impact of the assumed decrease in activity.</p>,
 	  "welcome5": <p>Try also to select CNN twice and then activate increased cost of CO<sub>2</sub> storage and compare CO<sub>2</sub>-reductions.</p>,
   },
   "tab5": {
-	  "welcome1": <p>Other sectors energy consumption.</p>,
+	  "welcome1": <div>Other sectors energy consumption.</div>,
 	  "welcome2": <p>Here you can investigate energy consumption and CO<sub>2</sub>-emissions from other sectors.</p>,
 	  "welcome3": <p>In Other sectors we include: Manufacturing industries, Service sector, Agriculture and Fishery.</p>,
 	  "welcome4": <p>Try to select CNN twice and then activate increased cost of CO<sub>2</sub> storage and compare CO<sub>2</sub>-reductions and fuel use in agriculture and fishery.</p>,
   },
   "tab6": {
-	  "welcome1": <p>Residential sector.</p>,
+	  "welcome1": <div>Residential sector.</div>,
 	  "welcome2": <p>Here you can investigate development in heat demand, electricity demand and fuel use for residential sector.</p>,
 	  "welcome3": <p>The results are split in room heat and electricity for appliances. The solutions for the residential sector are very robust towards the variation in assumptions are therefore all scenarios look very similar.</p>,
 	  "welcome4": <p>So instead of comparing two scenarios try to look at the difference in heating solutions between the countries. Turn on one country at the time on the map to the left to go through country results.</p>,
   },
   "tab7": {
-	  "welcome1": <p>Transport sector energy consumption.</p>,
+	  "welcome1": <div>Transport sector energy consumption.</div>,
 	  "welcome2": <p>Here you can investigate energy consumption, transport service level and CO<sub>2</sub>-emissions from passenger and freight transport.</p>,
 	  "welcome3": <p>The model includes international shipping and aviation which also can be shown on country level. For cars and trucks the stock are shown divided on vehicle types.</p>,
 	  "welcome4": <p>The results in the transport sector is very robust to the different scenario assumptions, which leads to only minor differences between the scenarios.</p>,
 	  "welcome5": <p>But the CNB scenarios differs as demand for transport are not assumed to increase in future. Therefore, try to select CNN and CNB and compare the need for cars.</p>,
   },
   "tab8": {
-	  "welcome1": <p>Key Performance Indicators.</p>,
+	  "welcome1": <div>Key Performance Indicators.</div>,
 	  "welcome2": <p>Here we have chosen some indicators that can say something about the development of the Nordic energy system.</p>,
 	  "welcome3": <p>The indicators show development in renewable energy shares, electrification of end-use energy consumption and energy intensity of industries.</p>,
 	  "welcome4": <p>The indicators illustrate differences in the structure of the energy system across the Nordic countries.</p>,
